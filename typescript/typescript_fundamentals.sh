a2ed4ca95d3254173719ec782e8cfcc885bc11b2
Jamaal Long <jamaal.long@zohomail.eu>
2022-02-04 21:50:24 -0600

    Add new questions and answers to typescript fundamentals

--- typescript/typescript_fundamentals.sh
+++ typescript/typescript_fundamentals.sh
@@ -3058,12 +3058,44 @@ answer=""
 evaluate_answer "$response" "$answer"
 
 
+# Given an array of items and an array of tests, this example determines whether all items pass all tests.
 
+# let allPass = true;
+# let i, j;
 
+# top:
+# for (i = 0; i < items.length; i++) {
+#   for (j = 0; j < tests.length; j++) {
+#     if (!tests[j].pass(items[i])) {
+#       allPass = false;
+#       break top;
+#     }
+#   }
+# }
 
+# print out the iterations
+# let i, j;
 
+# loop1:
+# for (i = 0; i < 3; i++) {      //The first for statement is labeled "loop1"
+#    loop2:
+#    for (j = 0; j < 3; j++) {   //The second for statement is labeled "loop2"
+#       if (i === 1 && j === 1) {
+#          continue loop1;
+#       }
+#       console.log('i = ' + i + ', j = ' + j);
+#    }
+# }
 
-
+# // Output is:
+# //   "i = 0, j = 0"
+# //   "i = 0, j = 1"
+# //   "i = 0, j = 2"
+# //   "i = 1, j = 0"
+# //   "i = 2, j = 0"
+# //   "i = 2, j = 1"
+# //   "i = 2, j = 2"
+# // Notice how it skips both "i = 1, j = 1" and "i = 1, j = 2"
 
 # comparison with OOP approach
 
